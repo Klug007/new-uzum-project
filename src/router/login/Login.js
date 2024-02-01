49d7ead236966846aa76fb72eeca4e0804cad4b1
Klug007 <tilla.yosinbek@gmail.com>
2024-02-01 20:21:31 +0500

    login

--- src/router/login/Login.js
+++ src/router/login/Login.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className="login">
+            <div className="login__form">
+                <form action="">
+                    <h2>Tizimga Kirish</h2>
+                    <label htmlFor="name">Фойдаланувчи Номи</label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Foydalanuvchi Nomi"
+                    />
+                    <label htmlFor="parol">Parol</label>
+                    <input
+                        type="password"
+                        name="parol"
+                        id=""
+                        placeholder="Parol"
+                    />
+                    <button>Kirish</button>
+                </form>
+                <p className="login__link">
+                    <a href="/">Assosiy sahifaga qaytish</a>
+                </p>
+            </div>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
