ce2b51fb038dcf0bb07563b70c5884fd346f494b
Klug007 <tilla.yosinbek@gmail.com>
2024-02-15 12:30:42 +0500

    admin sidebar

--- src/router/admin/Admin.js
+++ src/router/admin/Admin.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateProduct from "./create-product/CreateProduct";
 import ManageProduct from "./manage-product/ManageProduct";
 import { NavLink, Route, Routes, Link, useNavigate } from "react-router-dom";
+import { IoMdClose } from "react-icons/io";
+import { IoMenuOutline } from "react-icons/io5";
 import "./Admin.css";
 
 function Admin() {
-    const navigate = useNavigate()
+    const [open, setOpen] = useState(false);
+    const navigate = useNavigate();
     return (
         <div className="admin">
             <div className="admin__sidebar">
-                <button
-                    onClick={() => navigate("/")}
-                    className="admin__go-home"
-                >
-                    Go Home
-                </button>
-                <h2>Admin Panel</h2>
-                <NavLink to={"create-product"} className="admin__link">
-                    Create Product
-                </NavLink>
-                <NavLink to={"manage-product"} className="admin__link">
-                    Manage Product
-                </NavLink>
+                {
+                    open ? <IoMdClose onClick={() => setOpen(!open)} /> : <IoMenuOutline
+                    onClick={() => setOpen(!open)}
+                    className={open ? "close" : "open"}
+                />
+                }
+                
+                <div className={open ? `open` : "close"}>
+                    <button
+                        onClick={() => navigate("/")}
+                        className="admin__go-home"
+                    >
+                        Go Home
+                    </button>
+                    <h2>Admin Panel</h2>
+                    <NavLink to={"create-product"} className="admin__link">
+                        Create Product
+                    </NavLink>
+                    <NavLink to={"manage-product"} className="admin__link">
+                        Manage Product
+                    </NavLink>
+                </div>
             </div>
             <div className="admin__content">
                 <Routes>
